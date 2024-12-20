b3123fa3ca4f3be530b330c4bc32b26fd447d20a
Matteo Collina <hello@matteocollina.com>
2024-12-20 16:53:07 +0100

    working
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # watt-admin
+
+## Project setup
+
+```
+npm install
+```
+
+### Build
+
+```
+npm run build
+```
+
+### Start
+
+```
+npm run start
+```
+
+### Test the frontend app
+
+1. run the server
+2. open the browser and go to `http://localhost:3042/`
+3. click on `/admin` link
+4. this is the frontend app!
+
+### Test the backend app
+
+1. run the server
+2. open the browser and go to `http://localhost:3042/`
+3. click on "OpenAPI Documentation" link
+4. call the `/runtimes` endpoint
+5. call the `/metrics/{id}` endpoint
+
--- web/backend/routes/root.ts
+++ web/backend/routes/root.ts
@@ -1,5 +1,7 @@
 /// <reference path="../global.d.ts" />
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+// TODO: create types for RuntimeApiClient
+import { RuntimeApiClient } from '@platformatic/control'
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -8,7 +10,13 @@ declare module 'fastify' {
 }
 
 export default async function (fastify: FastifyInstance, opts: FastifyPluginOptions) {
+  const api = new RuntimeApiClient()
   fastify.get('/example', async (request, reply) => {
     return { hello: fastify.example }
+  })
+
+  fastify.get('/runtimes', async (request, reply) => {
+    const runtimes = await api.getRuntimes()
+    return runtimes
   })
 }
