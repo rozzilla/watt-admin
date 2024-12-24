1ff605ff06f45e9e4421fad590a581cbdbe1fcea
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-12-24 17:27:19 +0100

    feat(schema): implement types based on openapi
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- web/backend/routes/root.ts
+++ web/backend/routes/root.ts
@@ -1,5 +1,6 @@
 /// <reference path="../global.d.ts" />
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
 // TODO: create types for RuntimeApiClient and re-enable strict mode
 import { RuntimeApiClient } from '@platformatic/control'
 
@@ -10,13 +11,13 @@ declare module 'fastify' {
 }
 
 export default async function (fastify: FastifyInstance, opts: FastifyPluginOptions) {
+  const typedFastify = fastify.withTypeProvider<JsonSchemaToTsProvider>()
   const api = new RuntimeApiClient()
   fastify.get('/example', async (request, reply) => {
     return { hello: fastify.example }
   })
 
-  // TODO: add types using typebox
-  fastify.get('/runtimes', {
+  typedFastify.get('/runtimes', {
     schema: {
       querystring: {
         type: 'object',
@@ -31,10 +32,8 @@ export default async function (fastify: FastifyInstance, opts: FastifyPluginOpti
   }, async (request, reply) => {
     let runtimes = await api.getRuntimes()
 
-    // @ts-ignore
     console.log(request.query.includeAdmin)
 
-    // @ts-ignore
     if (!request.query.includeAdmin) {
       runtimes = runtimes.filter((runtime) => runtime.packageName !== 'watt-admin')
     }
@@ -42,8 +41,11 @@ export default async function (fastify: FastifyInstance, opts: FastifyPluginOpti
     return runtimes
   })
 
-  fastify.get('/runtimes/:pid/metrics', async (request, reply) => {
-    // @ts-ignore
+  typedFastify.get('/runtimes/:pid/metrics', {
+    schema: {
+      params: { type: 'object', properties: { pid: { type: 'number' } }, required: ['pid'] }
+    }
+  }, async (request, reply) => {
     return api.getRuntimeMetrics(request.params.pid, { format: 'json' })
   })
 }
