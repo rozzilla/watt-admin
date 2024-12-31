966354d002a6bdf867df7bcb3d0403473102fa6f
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-12-31 10:29:05 +0100

    chore(test): update
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- web/backend/hooks/close.ts
+++ web/backend/hooks/close.ts
@@ -2,7 +2,7 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
 export default async function (fastify: FastifyInstance, opts: FastifyPluginOptions) {
   fastify.addHook('onClose', async () => {
-    // FIXME: this is an issue on wattpm, since graceful shutdown time of 10s isn't considered (https://docs.platformatic.dev/docs/runtime/configuration#gracefulshutdown)
+    // FIXME: this is an issue on wattpm, since graceful shutdown time of 10s isn't considered (https://github.com/platformatic/platformatic/issues/3751)
     fastify.log.info('Closing the backend...')
     clearInterval(fastify.metricsInterval)
   })
--- web/backend/test/plugins/metrics.test.ts
+++ web/backend/test/plugins/metrics.test.ts
@@ -1,7 +1,11 @@
 import test from 'node:test'
 import assert from 'node:assert'
+import { getServer } from '../helper'
 
+// TODO: add setup and test on a up & running wattpm instance
 test('metrics plugin', async (t) => {
-  // FIXME: add real tests to the metrics plugin
-  assert.ok(true)
+  const server = await getServer(t)
+
+  assert.ok('_idleTimeout' in server.metricsInterval, 'interval for metrics are defined')
+  assert.deepEqual(server.mappedMetrics, {}, 'mapped metrics are defined')
 })
