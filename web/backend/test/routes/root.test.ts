9f3037780e3ce5721eb8bc05ee3eda0ee2f14e99
Roberto Bianchi <roberto.bianchi@spendesk.com>
2025-01-08 10:56:32 +0100

    chore(test): add more test (#12)
    * chore(test): add more test
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    * minor
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    * chore(test): add new
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    ---------
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- web/backend/test/routes/root.test.ts
+++ web/backend/test/routes/root.test.ts
@@ -2,6 +2,8 @@ import test from 'node:test'
 import assert from 'node:assert'
 import { getServer, startWatt } from '../helper'
 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 test('no runtime running', async (t) => {
   const server = await getServer(t)
   const res = await server.inject({
@@ -10,6 +12,13 @@ test('no runtime running', async (t) => {
   })
   assert.strictEqual(res.statusCode, 200)
   assert.deepStrictEqual(res.json(), [], 'with no runtime running')
+
+  const services = await server.inject({
+    method: 'GET',
+    url: '/runtimes/42/services'
+  })
+  assert.strictEqual(services.statusCode, 500)
+  assert.ok(services.json().message.includes('connect ENOENT'), 'unable to list services due to no runtime available')
 })
 
 test('runtime is running', async (t) => {
@@ -19,9 +28,55 @@ test('runtime is running', async (t) => {
     method: 'GET',
     url: '/runtimes'
   })
-  assert.strictEqual(res.statusCode, 200)
+  assert.strictEqual(res.statusCode, 200, 'runtimes endpoint')
   const [runtime] = res.json()
+  const runtimePid = runtime.pid
+  assert.strictEqual(runtime.packageName, 'backend')
   assert.strictEqual(typeof runtime.packageName, 'string')
-  assert.strictEqual(typeof runtime.pid, 'number')
+  assert.strictEqual(typeof runtimePid, 'number')
   assert.strictEqual(runtime.packageVersion, null)
+
+  const metricsEmpty = await server.inject({
+    method: 'GET',
+    url: `/runtimes/${runtimePid}/metrics`
+  })
+  assert.strictEqual(metricsEmpty.statusCode, 200, 'metrics endpoint')
+  assert.deepEqual(metricsEmpty.json(), [], 'metrics result is empty')
+
+  const services = await server.inject({
+    method: 'GET',
+    url: `/runtimes/${runtimePid}/services`
+  })
+  assert.strictEqual(services.statusCode, 200, 'services endpoint')
+  const servicesJson = services.json()
+  assert.strictEqual(servicesJson.production, true)
+  assert.strictEqual(servicesJson.entrypoint, 'backend')
+  assert.strictEqual(typeof servicesJson.services[0].url, 'string')
+  assert.strictEqual(typeof servicesJson.services[0].entrypoint, 'boolean')
+
+  // Wait for the interval to be run
+  await wait(1000)
+  const metrics = await server.inject({
+    method: 'GET',
+    url: `/runtimes/${runtimePid}/metrics`
+  })
+  assert.strictEqual(metrics.statusCode, 200, 'metrics endpoint')
+  const metricsJson = metrics.json()
+  assert.strictEqual(metricsJson[0].pid, runtimePid)
+  assert.strictEqual(metricsJson[0].serviceId, 'backend')
+  assert.notDeepEqual(metricsJson, [], 'metrics are not empty after the interval')
+
+  const serviceMetrics = await server.inject({
+    method: 'GET',
+    url: `/runtimes/${runtimePid}/metrics/backend`
+  })
+  assert.strictEqual(serviceMetrics.statusCode, 200, 'service metrics endpoint')
+  assert.notDeepEqual(serviceMetrics.json(), [], 'service metrics are not empty for a valid service name')
+
+  const serviceMetricsEmpty = await server.inject({
+    method: 'GET',
+    url: `/runtimes/${runtimePid}/metrics/fantozzi`
+  })
+  assert.strictEqual(serviceMetricsEmpty.statusCode, 200, 'service metrics endpoint')
+  assert.deepEqual(serviceMetricsEmpty.json(), [], 'service metrics are empty for an invalid service name')
 })
