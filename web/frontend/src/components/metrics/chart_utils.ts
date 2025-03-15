4f8e8399aff8ce162fd6a2fe0660a2f20d7514f0
Roberto Bianchi <roberto.bianchi@spendesk.com>
2025-03-15 20:32:44 +0100

    update
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

---
+++ web/frontend/src/components/metrics/chart_utils.ts
@@ -0,0 +1,37 @@
+interface SVGPathElement {
+  getTotalLength(): number;
+  getPointAtLength(length: number): DOMPoint;
+}
+
+interface DOMPoint {
+  x: number;
+  y: number;
+}
+
+// iteratively search a path to get a point close to a desired x coordinate
+export function findY(
+  path: SVGPathElement,
+  pathLength: number,
+  x: number,
+  width: number
+): number {
+  const accuracy = 1; // px
+  const iterations = Math.ceil(
+    Math.log10(accuracy / width) / Math.log10(0.5)
+  ); // for width (w), get the # iterations to get to the desired accuracy, generally 1px
+  
+  let nextLengthChange = pathLength / 2;
+  let nextLength = pathLength / 2;
+  let y = 0;
+  
+  for (let i = 0; i < iterations; i++) {
+    const pos = path.getPointAtLength(nextLength);
+    y = pos.y;
+    nextLength = x < pos.x 
+      ? nextLength - nextLengthChange 
+      : nextLength + nextLengthChange;
+    nextLengthChange = nextLengthChange / 2;
+  }
+  
+  return y;
+}
