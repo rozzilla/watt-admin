7b6b96645848681a129a5634bda86ec7c76a0f47
Roberto Bianchi <roberto.bianchi@spendesk.com>
2025-06-02 22:19:30 +0200

    fix(env): do not load project env
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- web/backend/utils/client.openapi.ts
+++ web/backend/utils/client.openapi.ts
@@ -3,7 +3,7 @@ import { buildServer } from '@platformatic/service'
 
 async function clientOpenapi () {
   const server = await buildServer({
-    $schema: 'https://schemas.platformatic.dev/@platformatic/service/2.60.0.json',
+    $schema: 'https://schemas.platformatic.dev/@platformatic/service/2.67.0.json',
     service: {
       openapi: true
     },
